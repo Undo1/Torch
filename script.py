54185f85cada4a7bf8d0853a5c6faa1cad0edddb
Undo1 <parker@erwaysoftware.com>
2014-04-24 17:31:55 -0600

    get hrefs

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 import mysql.connector
 import config #Where we keep our passwords and stuff
+
+cnx = mysql.connector.connect(user=config.MySQLUsername(), password=config.MySQLPassword(), host=config.MySQLHost(), database=config.MySQLDatabase())
+cursor = cnx.cursor()
+
+query = ("SELECT Body, Score FROM Posts WHERE PostTypeId=2 LIMIT 10")
+
+cursor.execute(query)
+
+for (Body, Score) in cursor:
+	soup = BeautifulSoup(Body)
+	for link in soup.findAll('a'):
+	    print(link.get('href'))
+
+cursor.close()
+cnx.close()
